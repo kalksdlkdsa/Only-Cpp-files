6d1fc5a464c51433dfb77c50d9e9a659a09d892f
kalksdlkdsa <156580271+kalksdlkdsa@users.noreply.github.com>
2024-01-15 17:36:45 +0700

    My first program (first version)

---
+++ My_C++_program.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    int tuoi = 0;
+    cout << tuoi << endl;
+    tuoi = 18;
+    char xyz = 'T';
+    cout << tuoi << endl;
+    cout << xyz << endl;
+    bool logic = true;
+    bool logic2 = false;
+    cout << logic << endl;
+    cout << logic2 << endl;
+    int xyz2[5] = {2,3,2,1,5};
+    cout << xyz2[8] << endl;
+    char ho_ten[100] = {'K','h','a','n','g'};
+    cout << ho_ten << endl;
+    string ho_ten_2 = "Khang";
+    cout << ho_ten_2 << endl;
+    return 0;
+}
